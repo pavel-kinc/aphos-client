37785044cfa5032cb669f0d153beda4739826ca4
Pavel Kinc <pavelkinc230@gmail.com>
2023-03-30 17:52:27 +0200

    feat(setup.py): Changing astropy requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aphos_openapi",
-    version="2.1.8",
+    version="2.1.9",
     author="Pavel Kinc",
     description="APhoS Python library for data representation",
     long_description=long_description,
@@ -22,6 +22,6 @@ setuptools.setup(
         'urllib3>=1.25.3',
         'python-dateutil',
         'matplotlib',
-        'astropy'
+        'astropy>=4.3'
     ]
 )
