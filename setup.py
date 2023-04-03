fc76ebfeeac7d158a5cc2727e78cdcc778fa963b
Pavel Kinc <pavelkinc230@gmail.com>
2023-04-03 14:13:17 +0200

    feat(release): New release 2.1.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aphos_openapi",
-    version="2.1.10",
+    version="2.1.11",
     author="Pavel Kinc",
     description="APhoS Python library for data representation",
     long_description=long_description,
